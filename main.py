0b4ffdb340144775fa2459231710fc5db20f1469
JonathanOgboru <62063539+jojogamezHub@users.noreply.github.com>
2022-03-02 15:23:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+cap = cv2.VideoCapture("/home/pax/test320x180.mp4")
+okay, frame = cap.read()
+fnum = 0
+print(f'# Video appears to be {len(frame[0])} x {len(frame)}')
+
+import requests
+
+
+# Process each frame.
+
+while True do
+response = requests.get()
+
+
+  
